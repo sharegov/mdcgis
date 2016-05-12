6e67c2ef20a4cb285c69dca08a1e0251c7703e63
Jorge Fiallega <jorgefiallega@gmail.com>
2016-05-12 12:22:52 -0400

    application config parameters for different profiles moved to the profiles where they belong.

---
+++ src/main/groovy/org/sharegov/mdcgis/ApplicationConfig.groovy
@@ -0,0 +1,13 @@
+package org.sharegov.mdcgis
+
+
+/**
+ * Configuration values needed to start up the application.
+ * @author fiallega
+ *
+ */
+class ApplicationConfig {
+	String keyStorePassword;
+	String keyStorePath;
+	String rootUri;
+}
